eaa160ea3c56994a9e3f3a76874ef5c363f092f9
Andrii <zakordonskyii@gmail.com>
2025-10-07 22:54:26 +0200

    Add NewsletterSubscription Repository

---
+++ dao/src/main/java/greencity/repository/NewsletterSubscriptionRepo.java
@@ -0,0 +1,10 @@
+package greencity.repository;
+
+import greencity.entity.NewsletterSubscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface NewsletterSubscriptionRepo extends JpaRepository<NewsletterSubscription, Long> {
+    Optional<NewsletterSubscription> findByEmail(String email);
+}
